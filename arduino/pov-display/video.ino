ac21ea1171c0862a2f12345ac385e366eadd23d2
yuval-dimri <yuval5363@gmail.com>
2023-08-30 15:14:26 +0300

    video abstraction goes here

---
+++ arduino/pov-display/video.ino
@@ -0,0 +1,8 @@
+// class videoFrame
+// {
+//   public:
+//   uint32_t timeForNextRow;
+//   uint nextRowIndex;
+//   uint32_t rowData[STRIP_NUM_LEDS];
+//   uint32_t frameData[IMAGE_WIDTH][STRIP_NUM_LEDS];
+// };
