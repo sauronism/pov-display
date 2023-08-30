20c3d8d3fac71d65e9a30f455d23ad738797a4f6
yuval-dimri <yuval5363@gmail.com>
2023-08-30 18:45:35 +0300

    basic image display logic (calculating imageCurser)

--- arduino/pov-display/pov-display.ino
+++ arduino/pov-display/pov-display.ino
@@ -1,12 +1,30 @@
+include<FastLED.h>
 
+#define IMAGE_HEIGHT 80          // in pixels
+#define IMAGE_HEIGHT_DEGREES 45  // image height in degrees (like Field Of View)
+#define NUM_LEDS 300
 
-#define IMAGE_WIDTH 80
+#define VIDEO_FPS 20
 
-void setup() {
-  encoderSetup(); //interrupt pin definition
+  void setup() {
+  encoderSetup();  //interrupt pin definition
 }
 
+uint32_t imageFrame[IMAGE_HEIGHT][NUM_LEDS];
 void loop() {
-  unsigned int rotationSpeed = calculateRotationSpeed();
+  int ledsAngle = encoderGetPosition();  // in degrees from the top
 
+  // TODO
+  // create a displayFrame(ImageFrame)
+  // -- nonblocking
+  // -- only updates the imageParser (TBD) with a new image to show.
+  // -- should be called 20 times per second (every 1000/VIDEO_FPS ms)
+  EVERY_N_MILLISECONDS(1000 / VIDEO_FPS) {
+    imageFrame = updateImageFrame();
+  }
+
+  int imageCurserY = (double)(360 / ledsAngle) * IMAGE_HEIGHT_DEGREES;  //starting from the top
+  ledStrip = imageFrame[imageCurserY];
+
+  
 }
