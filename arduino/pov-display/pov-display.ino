673cd51e94593fdbd650715e83fd726b6b62b880
yuval-dimri <yuval5363@gmail.com>
2023-08-31 21:14:26 +0300

    added leds decleration

--- arduino/pov-display/pov-display.ino
+++ arduino/pov-display/pov-display.ino
@@ -1,30 +1,33 @@
-include<FastLED.h>
+#include <FastLED.h>
 
 #define IMAGE_HEIGHT 80          // in pixels
 #define IMAGE_HEIGHT_DEGREES 45  // image height in degrees (like Field Of View)
-#define NUM_LEDS 300
 
 #define VIDEO_FPS 20
 
-  void setup() {
+void setup() {
+  delay(3000);  // 3 second delay for recovery
+
+  ledsSetup();
   encoderSetup();  //interrupt pin definition
 }
 
 uint32_t imageFrame[IMAGE_HEIGHT][NUM_LEDS];
+
+
 void loop() {
   int ledsAngle = encoderGetPosition();  // in degrees from the top
 
-  // TODO
-  // create a displayFrame(ImageFrame)
-  // -- nonblocking
-  // -- only updates the imageParser (TBD) with a new image to show.
-  // -- should be called 20 times per second (every 1000/VIDEO_FPS ms)
   EVERY_N_MILLISECONDS(1000 / VIDEO_FPS) {
-    imageFrame = updateImageFrame();
+    // change the image being used.
+    // this func creates the next image.
+    // TODO make sure it's fast
+    updateImageFrame(imageFrame);
   }
 
-  int imageCurserY = (double)(360 / ledsAngle) * IMAGE_HEIGHT_DEGREES;  //starting from the top
+  int imageCurserY = (double)(360 / ledsAngle) * IMAGE_HEIGHT_DEGREES;  // get current leds pos. starting from the top.
   ledStrip = imageFrame[imageCurserY];
 
-  
+
+  updateLedStrip();
 }
