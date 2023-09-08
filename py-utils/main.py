030f7d70c874d0da658c3c26d896334b8e71a1e4
yuval-dimri <yuval5363@gmail.com>
2023-09-08 17:45:16 +0300

    main.py works fine

--- py-utils/main.py
+++ py-utils/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import cv2
 from video_utils import VideoReader
 
@@ -8,7 +9,7 @@ video_reader_settings = {
     "video_path": "sauron.mp4",
     "width": 300,
     "height": 200,
-    "duration_seconds": 120,
+    "duration_seconds": 160,
     "fps": 20,
 }
 
@@ -17,9 +18,9 @@ def main():
     os.makedirs(output_folder, exist_ok=True)
 
     with VideoReader(**video_reader_settings) as video_reader:
-        for frame in video_reader.iterate_with_progress():
+        for frame_index, frame in enumerate(video_reader.iterate_with_progress):
             # for now, only save the file.
-            file_name = f"frame_{video_reader.current_frame}.bmp"
+            file_name = f"frame_{frame_index}.bmp"
             output_path = os.path.join(output_folder, file_name)
             cv2.imwrite(output_path, frame)
 
