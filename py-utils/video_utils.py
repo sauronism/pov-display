027c10af3b082c9a0abc8c2574a3d21657a0e57a
yuval-dimri <yuval5363@gmail.com>
2023-09-08 17:44:38 +0300

    video_utils is a usful shit

--- py-utils/video_utils.py
+++ py-utils/video_utils.py
@@ -4,19 +4,28 @@ from tqdm import tqdm
 
 class VideoReader:
     def __init__(self, video_path, width=None, height=None, duration_seconds=None, fps=None):
-        self.video_path = video_path
-        self.cap = cv2.VideoCapture(video_path)
         self.width = width
         self.height = height
         self.desired_fps = fps
-        self.current_frame = 0
 
-        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.original_fps = int(self.cap.get(cv2.CAP_PROP_FPS))
-        self.desired_duration_frames = int(
-            duration_seconds * self.original_fps)
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(video_path)
 
-        self.frame_skip_factor = self.original_fps // self.desired_fps
+        self.original_video_frame_count = int(
+            self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.original_video_fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+        if self.desired_fps > self.original_video_fps:
+            raise ValueError(
+                f"Original video is {self.original_video_fps}-fps.\n"
+                f"In your video, you want {self.desired_fps}-fps. Cannot make this up!!"
+            )
+
+        self.desired_frame_count = int(
+            duration_seconds * self.desired_fps)
+        self.frame_skip_factor = self.original_video_fps // self.desired_fps
+
+        self._current_frame = 0
+        self._output_current_frame = 0
 
         if not self.cap.isOpened():
             raise ValueError("Error: Couldn't open video file.")
@@ -25,15 +34,19 @@ class VideoReader:
         return self
 
     def __next__(self):
-        while self.current_frame < self.desired_duration_frames:
+        while self._output_current_frame < self.desired_frame_count:
             ret, frame = self.cap.read()
+            frame = cv2.resize(
+                frame, (self.width, self.height))
+
             if not ret:
                 self.cap.release()
                 raise StopIteration
 
-            self.current_frame += 1
+            self._current_frame += 1
 
-            if self.current_frame % self.frame_skip_factor == 0:
+            if self._current_frame % self.frame_skip_factor == 0:
+                self._output_current_frame += 1
                 break
 
         else:
@@ -48,6 +61,7 @@ class VideoReader:
     def __exit__(self, exc_type, exc_value, traceback):
         self.cap.release()
 
+    @property
     def iterate_with_progress(self):
-        for frame in tqdm(self, total=self.desired_duration_frames, desc="Processing Frames"):
+        for frame in tqdm(self, total=self.desired_frame_count, desc="Processing frames", unit="frames"):
             yield frame
