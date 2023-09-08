00dfe049d08646770e19499bad7b7bd824dded88
yuval-dimri <yuval5363@gmail.com>
2023-09-08 14:19:11 +0300

    added ledsAngle -> yCurser conversion function

--- arduino/pov-display/pov-display.ino
+++ arduino/pov-display/pov-display.ino
@@ -1,7 +1,10 @@
 #include <FastLED.h>
 
-#define IMAGE_HEIGHT 80          // in pixels
-#define IMAGE_HEIGHT_DEGREES 45  // image height in degrees (like Field Of View)
+#define IMAGE_HEIGHT_PIXELS 80    // in pixels
+#define IMAGE_SPREAD_DEGREES 160  // image height in degrees (like Field Of View)
+#define IMAGE_START_ANGLE 10
+#define IMAGE_END_ANGLE IMAGE_START_ANGLE + IMAGE_SPREAD_DEGREES
+
 
 #define VIDEO_FPS 20
 
@@ -12,22 +15,21 @@ void setup() {
   encoderSetup();  //interrupt pin definition
 }
 
-uint32_t imageFrame[IMAGE_HEIGHT][NUM_LEDS];
+uint32_t imageFrame[IMAGE_HEIGHT_PIXELS][NUM_LEDS];
 
 
 void loop() {
-  int ledsAngle = encoderGetPosition();  // in degrees from the top
+  int ledsAngle = encoderGetPosition();  // in degrees from the top TEST:(0 - 360)
 
   EVERY_N_MILLISECONDS(1000 / VIDEO_FPS) {
     // change the image being used.
     // this func creates the next image.
-    // TODO make sure it's fast
-    updateImageFrame(imageFrame);
+    // TEST make sure it's fast
+    getNewImageFrame(imageFrame);
   }
 
-  int imageCurserY = (double)(360 / ledsAngle) * IMAGE_HEIGHT_DEGREES;  // get current leds pos. starting from the top.
+  int imageCurserY = ledsAngleToYCurser(ledsAngle);
+
   ledStrip = imageFrame[imageCurserY];
-
-
   updateLedStrip();
 }
