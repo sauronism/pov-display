bd41aa7729f47153c7164da3721b1dcd43ab5844
yuval-dimri <yuval5363@gmail.com>
2023-09-08 16:12:37 +0300

    now main.py actually saves the files

--- py-utils/main.py
+++ py-utils/main.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 from video_utils import VideoReader
 
 output_folder = 'output_images/'
@@ -14,10 +15,14 @@ video_reader_settings = {
 
 def main():
     os.makedirs(output_folder, exist_ok=True)
+
     with VideoReader(**video_reader_settings) as video_reader:
         for frame in video_reader.iterate_with_progress():
-            # Save the frame as an image
-            output_path = os.path.join(
-                output_folder, f"frame_{video_reader.current_frame:04d}.bmp")
-
+            # for now, only save the file.
+            file_name = f"frame_{video_reader.current_frame}.bmp"
+            output_path = os.path.join(output_folder, file_name)
             cv2.imwrite(output_path, frame)
+
+
+if __name__ == "__main__":
+    main()
