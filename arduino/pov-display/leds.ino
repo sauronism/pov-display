24be1f7d6dff2039442b6b5bb70b41cafa8199cf
yuval-dimri <yuval5363@gmail.com>
2023-09-16 19:19:35 +0300

    moving declarations to main cpp file

--- arduino/pov-display/leds.ino
+++ arduino/pov-display/leds.ino
@@ -1,16 +1,4 @@
-// --- leds_hal --- //
 
-FASTLED_USING_NAMESPACE
-
-
-#define DATA_PIN 3
-#define CLK_PIN 4
-#define LED_TYPE APA102
-#define COLOR_ORDER GRB
-#define NUM_LEDS 300
-CRGB ledStrip[NUM_LEDS];
-
-#define BRIGHTNESS 100
 
 void ledsSetup() {
   FastLED.addLeds<LED_TYPE, DATA_PIN, CLK_PIN, COLOR_ORDER>(ledStrip, NUM_LEDS).setCorrection(TypicalLEDStrip);
--- arduino/pov-display/pov-display.ino
+++ arduino/pov-display/pov-display.ino
@@ -1,35 +1,59 @@
 #include <FastLED.h>
+#include <SD.h>
+#include <SPI.h>
 
+// --- LOGGING --- //
+#define LOG_SD true
+
+// --- image details --- //
 #define IMAGE_HEIGHT_PIXELS 80    // in pixels
 #define IMAGE_SPREAD_DEGREES 160  // image height in degrees (like Field Of View)
 #define IMAGE_START_ANGLE 10
 #define IMAGE_END_ANGLE IMAGE_START_ANGLE + IMAGE_SPREAD_DEGREES
 
-
+// --- video details --- //
 #define VIDEO_FPS 20
+#define ANIMATION_NUM_FRAMES 3200
+#define BASE_FILE_PATH "./animation1/img_"
+
+// --- leds --- //
+FASTLED_USING_NAMESPACE
+#define DATA_PIN 3
+#define CLK_PIN 4
+#define LED_TYPE APA102
+#define COLOR_ORDER GRB
+#define NUM_LEDS 300
+
+CRGB ledStrip[NUM_LEDS];
+#define BRIGHTNESS 100
+
+// --- sd reading --- //
+const int imageBufferSize = 3 * NUM_LEDS * IMAGE_HEIGHT_PIXELS;
+PROGMEM byte imageBuffer[imageBufferSize];  // R G B
 
 void setup() {
   delay(3000);  // 3 second delay for recovery
 
+  SDSetup();
   ledsSetup();
-  encoderSetup();  //interrupt pin definition
+  encoderSetup();  // interrupt pin definition
 }
 
-uint32_t imageFrame[IMAGE_HEIGHT_PIXELS][NUM_LEDS];
-
-
+CRGB imageFrame[IMAGE_HEIGHT_PIXELS][NUM_LEDS];
+int frameIndex;
 void loop() {
   int ledsAngle = encoderGetPosition();  // in degrees from the top TEST:(0 - 360)
 
   EVERY_N_MILLISECONDS(1000 / VIDEO_FPS) {
-    // change the image being used.
-    // this func creates the next image.
-    // TEST make sure it's fast
-    getNewImageFrame(imageFrame);
+    frameIndex++;
+    if (frameIndex > ANIMATION_NUM_FRAMES)
+      frameIndex = 0;
+    updateImageFrame(frameIndex);
   }
 
   int imageCurserY = ledsAngleToYCurser(ledsAngle);
+  // ledStrip = imageFrame[imageCurserY];
+  memcpy(imageFrame[imageCurserY], ledStrip, sizeof(ledStrip)); // copy each row at the time to the leds
 
-  ledStrip = imageFrame[imageCurserY];
   updateLedStrip();
 }
