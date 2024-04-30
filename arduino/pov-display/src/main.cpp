61b30262fe3d1fcd74c74e5418bfb04fc9db563d
yuval-dimri <yuval5363@gmail.com>
2024-04-30 22:20:54 +0300

    a bit cleaner

--- arduino/pov-display/src/main.cpp
+++ arduino/pov-display/src/main.cpp
@@ -2,6 +2,8 @@
 #include <SD.h>
 #include <SPI.h>
 #include <math.h>
+#include <iterator>
+#include <algorithm>
 
 // --- LOGGING --- //
 #define LOG_SD true
@@ -54,7 +56,6 @@ void ledsSetup()
 
   FastLED.addLeds<LED_TYPE, DATA_PIN, CLK_PIN, COLOR_ORDER>(ledStrip, NUM_LEDS); //.setCorrection(TypicalLEDStrip);
 
-  // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 }
 
@@ -133,50 +134,38 @@ void encoderSetup()
 class LowPassFilter
 {
 public:
-  float alpha;
-  float old_x_value;
   LowPassFilter(float alpha)
   {
-    this->alpha = alpha;
+    this->_alpha = alpha;
   }
   float apply(float x)
   {
-    x = (x * alpha) + (old_x_value * (1 - alpha));
-    old_x_value = x;
+    x = (x * _alpha) + (_oldValue * (1 - _alpha));
+    _oldValue = x;
     return x;
   }
+
+private:
+  float _alpha;
+  float _oldValue;
 };
 
-LowPassFilter timesLP(0.01);
-double position_millis_counter;
-unsigned long millis_true_position_offset = 0;
-float k_offset = 0.1;
+LowPassFilter encoderSpdLP(0.01);
 float angle = 0;
 double precise_millis;
+float timeFromLastTick;
+float degPerMilli;
 float encoderGetPosition()
 {
-  // encoderGetRotationSpeed();
+  // calc speed
+  encoder.millisPerRotation = encoderSpdLP.apply(encoder.tickTime - encoder.oldTickTime);
+  degPerMilli = 360.0f / encoder.millisPerRotation;
 
-  unsigned long dt = encoder.tickTime - encoder.oldTickTime;
-  encoder.millisPerRotation = timesLP.apply(dt);
-  float deg_per_millis = 360.0f / encoder.millisPerRotation;
-
+  // calc position
   precise_millis = micros() / 1000.0f;
-  float time_from_last_tick = precise_millis - encoder.tickTime;
-  float angle_based_on_last_tick = deg_per_millis * time_from_last_tick;
-  angle = angle_based_on_last_tick;
-  // angle = (deg_per_millis + millis_true_position_offset) * precise_millis;
+  timeFromLastTick = precise_millis - encoder.tickTime;
+  angle = degPerMilli * timeFromLastTick;
   angle = fmod(angle, 180);
-
-  // millis_true_position_offset = k_offset * (angle_based_on_last_tick - angle);
-
-  // angle = encoder.millis_per_rotation position_millis_counter;
-  // double millis_per_rotation = encoderGetRotationSpeed()
-  // angle of the device from the top - in degrees
-
-  // angle = encoderGetRotationSpeed() * position_millis_counter; // min(360, encoderGetRotationSpeed() * (float)timeFromLastTick);
-
-  // angle = angle > 180 ? angle - 180 : angle; // faster that angle % 180
 
   return angle;
 }
@@ -186,69 +175,28 @@ int ledsAngleToYCurser(float angle)
   angle -= IMAGE_START_ANGLE; // some offset, so 10 degrees is our starting position.
   angle = constrain(angle, 0, IMAGE_SPREAD_DEGREES);
 
-  int yPos = (angle / (float)IMAGE_SPREAD_DEGREES) * (float)IMAGE_HEIGHT_PIXELS;
+  int yCureser = (angle / (float)IMAGE_SPREAD_DEGREES) * (float)IMAGE_HEIGHT_PIXELS;
 
-  return yPos;
+  return yCureser;
 }
 
 void setup()
 {
-  delay(3000); // 3 second delay for recovery
+  delay(3000); // 3 second delay for good measure
 
   SDSetup();
   ledsSetup();
   encoderSetup(); // interrupt pin definition
-  // unsigned long start_time = micros();
+
+  // for tests only - read only one frame
   updateImageFrame(0);
   Serial.printf("setup end\n");
-
-  for (int i = 0; i < 300; i++)
-  {
-    ledStrip[i] = CRGB::Red;
-    FastLED.show();
-    delay(10);
-  }
-  for (int i = 0; i < 300; i++)
-  {
-    ledStrip[i] = CRGB::Black;
-    FastLED.show();
-    delay(10);
-  }
 }
 
-uint32_t color; // s[] = {CRGB::Black, CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::Yellow, CRGB::Purple};
-
-double millis_per_rotation_for_printing = 400;
 void loop()
 {
-  // if (encoder.millis_per_rotation > 60)
-  // {
-  //   if (encoder.millis_per_rotation < millis_per_rotation_for_printing)
-  //     millis_per_rotation_for_printing = encoder.millis_per_rotation;
-  // }
-  // if (millis() % 10000)
-  //   Serial.println(millis_per_rotation_for_printing);
 
   int ledsAngle = encoderGetPosition();
-  if (ledsAngle > IMAGE_END_ANGLE || ledsAngle < IMAGE_START_ANGLE)
-    fill_solid(ledStrip, NUM_LEDS, CRGB::Black);
-
-  // // int color_index = (ledsAngle / 180.0f) * 6;÷
-  // if (ledsAngle < 90)
-  //   color = CRGB::Black;
-  // else
-  //   color = CRGB::Red;
-  // fill_solid(ledStrip, NUM_LEDS, color);
-  // FastLED.show();
-
-  // int imageCurserY = ledsAngleToYCurser(ledsAngle);
-  // ledStrip[imageCurserY] = CRGB::Red;
-  // FastLED.show();
-
-  // ledStrip[constrain(ledsAngle, 0, 300)] = CRGB::Red;
-  // ledStrip[constrain(last_angle, 0, 300)] = CRGB::Black;
-
-  // last_angle = ledsAngle;
 
   // EVERY_N_MILLISECONDS(1000 / VIDEO_FPS)
   // {
@@ -259,12 +207,6 @@ void loop()
   // }
 
   int imageCurserY = ledsAngleToYCurser(ledsAngle);
-  // Serial.printf("imageCurserY: %d\n", imageCurserY);
-  // ledStrip = imageFrame[imageCurserY];
-  for (int i = 0; i < 300; i++)
-  {
-    ledStrip[i] = imageFrame[imageCurserY][i];
-  }
-  // // memcpy(imageFrame[imageCurserY], ledStrip, sizeof(ledStrip)); // copy each row at the time to the leds
+  std::copy(std::begin(imageFrame[imageCurserY]), std::end(imageFrame[imageCurserY]), std::begin(ledStrip));
   FastLED.show();
 }
